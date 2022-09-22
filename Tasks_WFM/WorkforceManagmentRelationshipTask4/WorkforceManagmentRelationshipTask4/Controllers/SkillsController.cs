3ea4a67d93170270c2ccf22a79ed31016390c568
Manju Priyaa <ManjupriyaaR@softura.com>
2022-09-22 19:49:27 +0530

    Task5-Approch1

---
+++ Tasks_WFM/WorkforceManagmentRelationshipTask4/WorkforceManagmentRelationshipTask4/Controllers/SkillsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Training_Task.ModelEntities;
+
+namespace WorkforceManagmentRelationshipTask4.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SkillsController : ControllerBase
+    {
+        private readonly ModelDbContext _context;
+
+        public SkillsController(ModelDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Skills
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Skills>>> GetSkills()
+        {
+            var result = await _context.Skills.Include(x => x.SkillMaps).ThenInclude(x => x.Skills).Select(x => new Skills_Employees
+            {
+                skillid = x.Id,
+                name = x.Name,
+                Employees = x.SkillMaps.Select(y => y.Employees.Name).ToList()
+            }).ToListAsync();
+            return result;
+        }
+
+    }
+}
---
+++ Tasks_WFM/WorkforceManagmentRelationshipTask4/WorkforceManagmentRelationshipTask4/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WorkforceManagmentRelationshipTask4.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
